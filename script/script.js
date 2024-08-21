2b681ebe7464bcef5cc082cb49382ce3a29c5c8d
Shordo <161672508+Shordo@users.noreply.github.com>
2024-08-21 15:35:00 -0600

    Primer commit del proyecto
    Se agrega la estructura de la página web junto con una versión temprana de los archivos CSS y JavaScript.
    Es necesario realizar las modificaciones pertinentes para que se alinee con los requerimientos del reto ya que no toma en cuenta las especificaciones del boceto ni la estructura de encriptación.

---
+++ script/script.js
@@ -0,0 +1,47 @@
+let textExist = false;
+function encryptText() {
+    const inputText = document.getElementById('input-text').value;
+    if (inputText == '') {
+        console.log("texto vacio");
+        document.getElementById('image-output').style.display = 'flex';
+        document.getElementById('output-text').innerText = "Texto encriptado/desencriptado aparecerá aquí...";
+        textExist = false;
+    }else{
+        /*
+            conversion de la cadena de texto
+        */
+        /* const encryptText = inputText;*/
+        document.getElementById('output-text').innerText = inputText;
+        document.getElementById('image-output').style.display = 'none';
+        document.getElementById('output-text').style.margin = '26px 0px';
+        textExist = true;
+    }
+}
+
+function decryptText() {
+    const inputText = document.getElementById('input-text').value;
+    if (inputText == '') {
+        console.log("texto vacio");
+        document.getElementById('image-output').style.display = 'flex';
+        document.getElementById('output-text').innerText = "Texto encriptado/desencriptado aparecerá aquí...";
+        textExist = false;
+    }else{
+        /*
+            desconversion de la cadena de texto
+        */
+        /*const decryptedText = inputText.value; */
+        document.getElementById('output-text').innerText = inputText;
+        document.getElementById('image-output').style.display = 'none';
+        textExist = true;
+    }
+}
+
+function copyText() {
+    if(textExist){
+        const outputText = document.getElementById('output-text').value;
+        navigator.clipboard.writeText(outputText).then(() => {
+        alert('Texto copiado al portapapeles');
+    });
+    }
+    console.log(textExist);    
+}
