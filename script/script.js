1806a31d8ee196141cb3c29ee9d1491eb026504c
Jordy Arriaga <acje1407@gmail.com>
2024-08-22 18:09:21 -0600

    Modificaciones al proyecto Estas modificaciones afectan a todo el proyecto agregando mas funcionalidades demandadas en el reto

--- script/script.js
+++ script/script.js
@@ -1,57 +1,68 @@
 let textExist = false;
 function encryptText() {
-    const inputText = document.getElementById('input-text').value;
+    let inputText = document.getElementById('input-text').value;
     if (inputText == '') {
         console.log("texto vacio");
         document.getElementById('output-text').innerText = inputText;
         document.getElementById('image-output').style.display = 'flex';
         document.getElementById('h2').style.display = 'flex';
         document.getElementById('p').style.display = 'flex';
+        document.getElementById('output-text').style.display = 'none';
         textExist = false;
-
     }else{
-        /*
-            conversion de la cadena de texto
-        */
-        /* const encryptText = inputText;*/
+        inputText = inputText.replace(/e/g, "enter");
+        inputText = inputText.replace(/i/g, "imes");
+        inputText = inputText.replace(/a/g, "ai");
+        inputText = inputText.replace(/o/g, "ober");
+        inputText = inputText.replace(/u/g, "ufat");
         document.getElementById('output-text').innerText = inputText;
         document.getElementById('image-output').style.display = 'none';
         document.getElementById('h2').style.display = 'none';
         document.getElementById('p').style.display = 'none';
-        document.getElementById('output-text').style.margin = '26px 0px';
+        document.getElementById('output-text').style.display ='flex';
+        document.getElementById('output-text').style.margin = '70px 0px 55px 0';
         textExist = true;
     }
 }
 
 function decryptText() {
-    const inputText = document.getElementById('input-text').value;
+    let inputText = document.getElementById('input-text').value;
     if (inputText == '') {
         console.log("texto vacio");
         document.getElementById('output-text').innerText = inputText;
         document.getElementById('image-output').style.display = 'flex';
         document.getElementById('h2').style.display = 'flex';
         document.getElementById('p').style.display = 'flex';
+        document.getElementById('output-text').style.display = 'none';
         textExist = false;
     }else{
-        /*
-            desconversion de la cadena de texto
-        */
-        /*const decryptedText = inputText.value; */
+        inputText = inputText.replace(/enter/g, "e");
+        inputText = inputText.replace(/imes/g, "i");
+        inputText = inputText.replace(/ai/g, "a");
+        inputText = inputText.replace(/ober/g, "o");
+        inputText = inputText.replace(/ufat/g, "u");
         document.getElementById('output-text').innerText = inputText;
         document.getElementById('image-output').style.display = 'none';
         document.getElementById('h2').style.display = 'none';
         document.getElementById('p').style.display = 'none';
-        document.getElementById('output-text').style.margin = '26px 0px';
+        document.getElementById('output-text').style.display ='flex';
+        document.getElementById('output-text').style.margin = '70px 0px 55px 0';
         textExist = true;
     }
 }
 
 function copyText() {
     if(textExist){
-        const outputText = document.getElementById('output-text').value;
-        navigator.clipboard.writeText(outputText).then(() => {
-        alert('Texto copiado al portapapeles');
-    });
+        let outputText = document.getElementById('output-text').value;
+        navigator.clipboard.writeText(outputText);
     }
-    console.log(textExist);    
 }
+
+
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
